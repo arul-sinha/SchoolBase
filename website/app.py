5cfbd2e52323b1b1341f6b42063368128dec4a2d
arul-sinha <arul-sinha@users.noreply.github.com>
2023-05-08 18:40:37 +0530

    users database create

--- website/app.py
+++ website/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, redirect, Response, render_template, request
+import os
+import cs50
+
+db_users = SQL("sqlite:///./sql/users.db")
 
 app = Flask("__name__")
 
@@ -14,6 +18,10 @@ def register():
     if request.method == 'GET':
         return render_template("register.html")
     else:
+        name = request.form.get("reg_name")
+        email = request.form.get("reg_email")
+        password = request.form.get("reg_pass")
+        password_confirm = request.form.get("reg_con_pass")
         return redirect("/login")
 
 if __name__ == "__main__":
